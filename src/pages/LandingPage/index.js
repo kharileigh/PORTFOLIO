7d2d5e74040ef0d49e929af81ea09e4290a4831a
kharileigh <kharileighmiller@gmail.com>
2022-07-19 14:55:24 +0100

    COMPLETED STYLING OF LANDING PAGE
    Final styling completed, functionality to be added next to lead user to the next page

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import TypeWriterEffect from 'react-typewriter-effect';
+
+import './Landing.css';
+import profile from '../../images/profile.png';
 
 const LandingPage = () => {
 
     return (
+
+        // ----- TO CENTER CONTENT TO MIDDLE OF PAGE ------ //
         <div className="main-container">
+
+            {/* ------ FOR IMAGE & TYPEWRITER | ALIGNED HORIZONTALLY ----- */}
+            <div className="content-container">
+             
+                
+                    
+                <img 
+                    fluid="true"
+                    src={profile}
+                    alt="Profile Picture"
+                    width='220'
+                    height='275'
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        alignSelf: 'center',
+                        marginRight: 'auto',
+                        borderRadius: '110px',
+                        padding: '25px',
+                    }}
+                />
+               
+
+                <div className='typewriter'>
+
+                    <TypeWriterEffect 
+                    
+                        textStyle={{
+                            color: 'white',
+                            fontWeight: 'bold',
+                            fontSize: '1.4em',
+                            lineHeight: '31px',
+                            padding: '5px',
+
+                            alignItems: 'center',
+                            display: 'flex',
+                            justifyContent: 'flex-start',
+                            textAlign: 'center',
+                            textShadow: '0px 0px 20px 1px rgba(18, 74, 167, 1)',
+                             
+                        }}
+                        
+                        multiText={[
+                            "Hi! I'm Khari!",
+                            "a spiritually conscious individual",
+                            "seeking to merge",
+                            "creativity, technology and purpose",
+                        ]}
+                        cursorColor='white'
+                        typeSpeed={45}
+                        startDelay={0.1}
+                        nextTextDelay={0.1}
+                        multiTextLoop={true}
+                    />
+   
+
+                </div>
+
+                <br></br>
+
+                <div className="button-container">
+                        <button type="button" 
+                                className="landing-button" 
+                                // onClick={handleEntry} 
+                        >
+                        <span className="text">Unpack My Story </span>
+                        </button>
+
+                </div> 
+                
+
+            </div> 
 
         </div>
     );
